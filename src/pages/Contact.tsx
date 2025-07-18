8f35d4970553190abb8273313ed685de6d38f26c
amanbhattarai34 <dukuaman@gmail.com>
2025-07-18 23:15:17 +0545

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Mail, Phone, MapPin, Linkedin, ExternalLink, Send, MessageCircle } from 'lucide-react';
+import { Mail, Phone, MapPin, Liaknkedin, ExternalLink, Send, MessageCircle } from 'lucide-react';
 
 const Contact = () => {
   return (
